13b65d75eed018abd8562488e5e65e1d42f00ae5
Andrei Arkhipov <a.arkhipov@hevelsolar.com>
2022-02-18 15:45:07 +0300

    lesson 6

--- src/script.js
+++ src/script.js
@@ -1,36 +1,18 @@
-//lesson 5
+//lesson 6
+let out = document.querySelector('.out');
+// for (let i = 0; i < 5; i++) {
 
-// for
-
-// let div = document.querySelectorAll('.out');
-// console.log(div);
-// div.style.backgroundColor = 'red';
-
-// for (let i = 0; i < div.length; i += 1) {
-//     // console.log(i);
-//     div[i].style.background = 'red';
-//     div[i].onclick = click;
+//     for (let k = 0; k < 5; k++) {
+//         out.innerHTML += '*';
+//     }
+//     out.innerHTML += '<br>';
 // }
 
-// function click() {
-//     console.log("Click!")
-// }
-
-// let b = document.getElementsByClassName('out');
-// console.log(b);
-// b[1].style.border = '1px solid green';
-
-// let c = document.getElementsByTagName('div');
-// console.log(c);
-
-document.querySelector('button').onclick = () => {
-    let r = document.querySelectorAll('input[type="radio"]');
-    console.log(r);
-    for (let i = 0; i < r.length; i++) {
-        if (r[i].checked) {
-            console.log(r[i].value);
-        }
+for (k = 1; k <= 9; k++) {
+    // out.innerHTML += k + "_";
+    for (let i = 1; i <= 9; i++) {
+        out.innerHTML += `${k} x ${i} = ${i * k} <br>`;
+        // out.innerHTML += i + ' ';
     }
-}
-
-// while
+    out.innerHTML += '<br>';
+}   
