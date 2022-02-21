e20e990be24598f8211e64dda2d2c835d16cd1f7
Andrei Arkhipov <a.arkhipov@hevelsolar.com>
2022-02-21 12:53:00 +0300

    lesson 8

--- src/script.js
+++ src/script.js
@@ -1,33 +1,29 @@
-//lesson 7
-// one();
+//lesson 8
+// let num1 = document.querySelector('.in-1');
+// let num2 = document.querySelector('.in-2');
 
 let but = document.querySelector('.btn');
+let divElem = document.querySelectorAll('div');
 
-// but.onclick = one;
+console.log(divElem);
 
-// function one() {
-//     console.log('Print')
-// }
+but.onclick = () => {
+    // console.log(num1.value);
+    // console.log(num2.value);
+    // let i = +num1.value;
+    // let k = +num2.value;
+    // console.log(i, k);
+    // while (i <= k) {
+    //     console.log(i);
+    //     document.querySelector('.out-1').innerHTML += i + ' ';
+    //     i++;
+    // }
+    let i = 1;
+    // console.log(divElem[i]);
+    while (i < divElem.length) {
+        divElem[i].style.background = 'orange';
+        i++;
+    }
 
-// function name(a) {
-//     return a;
-// }
-
-// (a) => 2 * a;
-
-// // console.log(1 + one()); // undefined function result
-
-// function two() {
-//     return 7;
-// }
-
-let a2 = 8;
-
-function t2() {
-    return '<b>Bold</b>';
 }
 
-document.querySelector('.btn').onclick = (event) => {
-    event.preventDefault();
-    document.querySelector('.out-1').textContent = t2();
-}
