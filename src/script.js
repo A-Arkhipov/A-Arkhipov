36a504f9ef1b8c1eb7ee15c4fbe717113f26482d
Andrei Arkhipov <a.arkhipov@hevelsolar.com>
2022-03-15 13:54:36 +0300

    fetch

--- src/script.js
+++ src/script.js
@@ -1,41 +1,14 @@
-//lesson 11
+//lesson 14
 
-const a = {
-    "a": 5,
-    "b": 'Hello',
-    "z2": 'Hi',
-    y43: 1999,
-    "villa de": 300,
-};
+fetch('http://api.openweathermap.org/data/2.5/weather?id=727762&appid=fbbad7f7215c3817ef463c36a436de61')
+    .then(function (resp) { return resp.json() })
+    .then(function (data) {
+        console.log(data);
+        document.querySelector('.city').textContent = data.name;
+        document.querySelector('.temp').innerHTML = Math.round(data.main.temp - 273) + '&deg';
+        document.querySelector('.disclaimer').textContent = data.weather[0]['description'];
+        document.querySelector('.feature li').innerHTML = `<img src="http://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">`;
+    })
+    .catch(function () {
 
-a.yyy = 555;
-
-// a.z2 = 111;
-
-// console.log(a.z2);
-
-// let k = 'b';
-// console.log(a[k]);
-
-delete a.a;
-
-console.log(a);
-
-let out = 0;
-
-for (let key in a) {
-    if (Number.isInteger(a[key])) {
-        out += a[key];
-    }
-    // out += key + ' - ' + a[key] + '<br>';
-    // if (key == 'y43') {
-    //     delete a[key];
-    // }
-}
-
-console.log(out);
-// outA(a);
-
-function outA(outA) {
-    document.querySelector('.out-1').innerHTML = outA;
-}
+    });
