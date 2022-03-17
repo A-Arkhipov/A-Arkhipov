8c0f39bbaf4962d3e2f0afab1b00a65fbe7241ba
Andrei Arkhipov <a.arkhipov@hevelsolar.com>
2022-03-17 10:27:34 +0300

    lesson 15

--- src/script.js
+++ src/script.js
@@ -1,14 +1,42 @@
-//lesson 14
+//lesson 15
 
-fetch('http://api.openweathermap.org/data/2.5/weather?id=727762&appid=fbbad7f7215c3817ef463c36a436de61')
-    .then(function (resp) { return resp.json() })
-    .then(function (data) {
-        console.log(data);
-        document.querySelector('.city').textContent = data.name;
-        document.querySelector('.temp').innerHTML = Math.round(data.main.temp - 273) + '&deg';
-        document.querySelector('.disclaimer').textContent = data.weather[0]['description'];
-        document.querySelector('.feature li').innerHTML = `<img src="http://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">`;
-    })
-    .catch(function () {
+// let a = new Set();
+// a.add(1);
+// a.add('text');
+// a.add(7);
+// a.add(1);
+// console.log(a);
+// console.log(a.keys());
 
-    });
+let m = [];
+for (let i = 0; i < 10; i++) {
+    m[i] = Math.round(Math.random() * 5);
+}
+
+console.log(m);
+
+let a = new Set(m);
+
+m = Array.from(a);
+
+console.log(m);
+
+// for (let i of a) {
+//     console.log(i);
+// }
+
+
+// const set1 = new Set();
+// set1.add({ x: 10, y: 20 }).add({ x: 20, y: 30 });
+
+// console.log(set1);
+
+// // Delete any point with `x > 10`.
+// set1.forEach((point) => {
+//     if (point.x > 20) {
+//         set1.delete(point);
+//     }
+// });
+
+// console.log(set1.has());
+// expected output: 1
